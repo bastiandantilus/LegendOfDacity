738080ef45e7568b89011dc24a330c5933fa0c74
Scott Robert Lawrence <theelven@gmail.com>
2013-04-15 17:50:58 -0700

    trimming back a bit

--- scripts/WeaponInstance.js
+++
@@ -1,9 +0,0 @@
-WeaponInstanceClass = EntityClass.extend({
-    damageMultiplier:1.0,
-
-    init: function (x, y, settings) {
-        this.parent(x, y, settings);
-    }
-});
-
-gGameEngine.factory.WeaponInstance = WeaponInstanceClass;
